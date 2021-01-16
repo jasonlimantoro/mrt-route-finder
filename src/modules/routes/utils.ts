693a2efd67f27cfa8fc8b8bc150fbe3bfa30d1bb
jasonlimantoro <jason@ewburgers.com>
2021-01-16 12:14:50 +0800

    fix: exculsive bound for upper bound for time

--- src/modules/routes/utils.ts
+++ src/modules/routes/utils.ts
@@ -5,8 +5,8 @@ import { Instruction, MrtMap, Route, StationID, StationType } from "./types";
 export const isPeak = (currentTime: Date) => {
 	const dayNumber = currentTime.getDay();
 	const hours = currentTime.getHours();
-	const morningPeak = hours >= 6 && hours <= 9;
-	const eveningPeak = hours >= 18 && hours <= 21;
+	const morningPeak = hours >= 6 && hours < 9;
+	const eveningPeak = hours >= 18 && hours < 21;
 	const weekDay = dayNumber >= 1 && dayNumber <= 5;
 	return weekDay && (morningPeak || eveningPeak);
 };
@@ -14,7 +14,7 @@ export const isPeak = (currentTime: Date) => {
 export const isNight = (currentTime: Date) => {
 	const hours = currentTime.getHours();
 	const night = hours >= 22 && hours <= 23;
-	const dawn = hours <= 6;
+	const dawn = hours < 6;
 	return night || dawn;
 };
 
